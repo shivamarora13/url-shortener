d19814367f4b8b88451a002196feb0842124bfa3
Shivam Arora <shivamarora1131@gmail.com>
2016-06-22 17:49:56 +0530

    done

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 var express = require('express');
+var mongo = require('mongodb').MongoClient;
 var app = express();
-
+var url = 'mongodb://user:password@ds021761.mlab.com:21761/shorturl';
+var validUrl = require('valid-url');
+var sequence = 0;
+var original_url = 'https://cryptic-reef-58313.herokuapp.com/';
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+mongo.connect(url,function(err,db){
+  if(err){
+    throw err;
+  }else{
+    console.log("Connected to db");
+  }
+  var Urls = db.collection('urls');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+  app.get('/', function(request, response) {
+    response.render(index.html);
+  });
+
+  app.get('/new/*',function(request,response){
+    var req_url = request.path.replace('/new/','');
+    if(!validUrl.isUri(url)){
+      response.json({Error : "URL Invalid"});
+    }else{
+      sequence++;
+      Urls.insertOne({"original_url":req_url, "short_url":original_url+sequence});
+      response.json({"original_url":req_url, "short_url":original_url+sequence});
+      response.end();
+    }
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  app.get('/*',function(request,response){
+    var num = request.path.replace('/','');
+    console.log(num);
+    Urls.find({"short_url":original_url+num}).limit(1).toArray(function(err,docs){
+      if(err){
+        response.json({Error : "URL Invalid"});
+        throw err;
+      }else{
+        console.log(docs);
+        response.redirect(docs[0].original_url);
+      }
+    });
+  });
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
 });
-
-
